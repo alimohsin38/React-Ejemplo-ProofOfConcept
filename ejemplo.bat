49a092ae0e8f5b460c7218901048376b170d9a0b
Mohsin Ali <ali.mohsin38@gmail.com>
2018-06-07 10:52:09 +0530

    sass commond prompt handled from batch file

--- ejemplo.bat
+++ ejemplo.bat
@@ -1,8 +1,8 @@
 @echo off 
 echo batch controller starting... & 
 :: There are three parameters to generate css from scss in compact version >> compact, compress, compressed... 
-::call scss ./scss/style.scss ./css/style.css --style compact & 
-start scss --watch scss/style.scss:css/style.css --style compressed & 
+::start scss --watch scss/style.scss:css/style.css --style compact & 
+call scss ./scss/style.scss ./css/style.css --style compressed &
 call npm start & 
 call npm run build & 
 call xcopy.exe index.html build & 
